a1bae0a8ce1f54f5c8076d144ea43fcfa2988526
VegardSkaansar <vegaskaa.stud@ntnu.no>
2019-04-02 11:15:16 +0200

    Adding onSensorChange and finding the x, y, z position, pluss add

--- app/src/main/java/e/vegard/virtualball/ThrowFragment.java
+++ app/src/main/java/e/vegard/virtualball/ThrowFragment.java
@@ -1,9 +1,14 @@
 package e.vegard.virtualball;
 
 
+import android.content.Context;
 import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -15,7 +20,11 @@ import android.view.ViewGroup;
 public class ThrowFragment extends Fragment {
 
 private MainActivity mainActivity;
-private Sensor accelerometer = mainActivity.mSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+private Sensor accelerometer;
+
+
+// const
+    private final String TAG = "ThrowFragment";
 
     public ThrowFragment() {
         // Required empty public constructor
@@ -30,6 +39,41 @@ private Sensor accelerometer = mainActivity.mSensorManager.getDefaultSensor(Sens
 
         // getting the activity for the fragment
         mainActivity = ((MainActivity)getActivity());
+
+        mainActivity.mSensorManager = (SensorManager) mainActivity.getSystemService(Context.SENSOR_SERVICE);
+
+        // setting sensor
+        accelerometer = mainActivity.mSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+
+        // adding event listener to the accelerometer
+        SensorEventListener eventListener = new SensorEventListener() {
+            @Override
+            public void onSensorChanged(SensorEvent event) {
+                double x = event.values[0];
+                double y = event.values[1];
+                double z = event.values[2];
+
+                double acc = mainActivity.acceleration(x, y, z);
+
+
+                //double time =
+
+
+                if(acc > 9.81) {
+                    Log.d(TAG, "acceleration: " + acc);
+
+
+                }
+
+            }
+
+            @Override
+            public void onAccuracyChanged(Sensor sensor, int accuracy) {
+
+            }
+        };
+
+        mainActivity.mSensorManager.registerListener(eventListener, accelerometer, mainActivity.mSensorManager.SENSOR_DELAY_NORMAL);
 
 
         return v;
