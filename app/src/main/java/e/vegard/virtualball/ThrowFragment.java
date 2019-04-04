42d1b12c03d11c17159acd7326f6366ba83008be
VegardSkaansar <vegaskaa.stud@ntnu.no>
2019-04-04 21:24:33 +0200

    updating the comments

--- app/src/main/java/e/vegard/virtualball/ThrowFragment.java
+++ app/src/main/java/e/vegard/virtualball/ThrowFragment.java
@@ -7,11 +7,13 @@ import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
 import android.os.Bundle;
+import android.os.CountDownTimer;
 import android.support.v4.app.Fragment;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
 
 import java.text.DecimalFormat;
@@ -27,6 +29,9 @@ public class ThrowFragment extends Fragment {
 private MainActivity mainActivity;
 private Sensor accelerometer;
 private double accBall;
+private Button btnThrow;
+
+private Boolean cooldown = true;
 
 
 // const
@@ -35,6 +40,7 @@ private double accBall;
     // variable for textviews
     private TextView second;
     private TextView dist;
+    private TextView viewCooldown;
 
     public ThrowFragment() {
         // Required empty public constructor
@@ -62,51 +68,102 @@ private double accBall;
         // setting the textviews
         second = v.findViewById(R.id.txt_seconds);
         dist = v.findViewById(R.id.txt_distance);
+        viewCooldown = v.findViewById(R.id.txt_countdown);
+
+        // setting button
+        btnThrow = v.findViewById(R.id.btn_startThrow);
+
 
         // adding event listener to the accelerometer
-        SensorEventListener eventListener = new SensorEventListener() {
+       final SensorEventListener eventListener = new SensorEventListener() {
+
+            // Functionality: We used it for accelerometer, and gets the position when the sensor change
+            //                position.
+            // Reason: To get the x, y, z position on change to calculate acceleration
+            // Extra:
             @Override
             public void onSensorChanged(SensorEvent event) {
+
+                if(cooldown) {
+                    setCountDown(this);
+                    cooldown = false;
+                }
+                
                 double x = event.values[0];
                 double y = event.values[1];
                 double z = event.values[2];
 
                 double acc = MathUtils.acceleration(x, y, z);
 
-
-                //double time =
-
                 Log.d(TAG, "acceleration: " + acc);
-                if(acc > 9.81) {
-
-                    mainActivity.mSensorManager.unregisterListener(this, accelerometer);
-                    setStats(acc);
-
-                        // Toast.makeText(getActivity(), "time: " + df2.format(time) + " distance: " + distance, Toast.LENGTH_LONG).show();
+                if(acc > accBall) {
+                    accBall = acc;
                 }
 
             }
+           // Functionality: no functionality
+           // Reason: has to be implemented because of SensorEventListener
+           // Extra:
+           @Override
+           public void onAccuracyChanged(Sensor sensor, int accuracy) {
 
-            @Override
-            public void onAccuracyChanged(Sensor sensor, int accuracy) {
-
-            }
+           }
         };
 
-        mainActivity.mSensorManager.registerListener(eventListener, accelerometer, mainActivity.mSensorManager.SENSOR_DELAY_NORMAL);
+        btnThrow.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Setting the listner when button is pressed
+                mainActivity.mSensorManager.registerListener(eventListener, accelerometer, mainActivity.mSensorManager.SENSOR_DELAY_NORMAL);
+                // Setting the button to disabled, reason users cant spam the button
+                btnThrow.setEnabled(false);
+                // cooldown functionality is too set the timer of 5 sek
+                cooldown = true;
+            }
+        });
 
 
         return v;
     }
 
+    // Functionality: Sets the textview time and distance view with math
+    // Reason: sets the textview text
+    // Extra:
     public void setStats(double acc) {
-        accBall = acc;
-        String time = MathUtils.secondsToPoint(accBall);
-        String distance = MathUtils.distanceTravelled(accBall);
+        String time = MathUtils.secondsToPoint(acc);
+        String distance = MathUtils.distanceTravelled(acc);
 
-        dist.setText(dist.getText().toString() + distance);
-        second.setText(second.getText().toString() + time);
+        dist.setText("Distance: " + distance);
+        second.setText("Seconds: " + time);
 
+    }
+
+    // Functionality: 5 seconds tick every sek, after 5 sek unregisterlistener
+    // Reason: to get the highest onsensorchanged value in this timeinterval
+    // Extra:
+    public void setCountDown(final SensorEventListener listener) {
+        new CountDownTimer(5000, 1000) {
+
+            // Functionality: Every tick update countdown
+            // Reason: Make a countdown, update a textview
+            // Extra:
+            @Override
+            public void onTick(long millisUntilFinished) {
+                viewCooldown.setText(String.valueOf(Math.round(millisUntilFinished * 0.001f)));
+            }
+
+            // Functionality: When the countdown is finished unregister listener
+            // Reason: Solve the problem with when user do the throw
+            @Override
+            public void onFinish() {
+                mainActivity.mSensorManager.unregisterListener(listener, accelerometer);
+                Log.d(TAG, "onFinish: done");
+                btnThrow.setEnabled(true);
+                setStats(accBall);
+                viewCooldown.setText("");
+
+            }
+        }.start();
     }
 
 }
