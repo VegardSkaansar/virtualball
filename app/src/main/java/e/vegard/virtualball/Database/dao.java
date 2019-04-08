0a42f691d84a852c0dee7b062be0dfa9edf12862
VegardSkaansar <vegaskaa.stud@ntnu.no>
2019-04-08 12:08:54 +0200

    Adding update for my room db, and delete

--- app/src/main/java/e/vegard/virtualball/Database/dao.java
+++ app/src/main/java/e/vegard/virtualball/Database/dao.java
@@ -4,6 +4,7 @@ import android.arch.lifecycle.LiveData;
 import android.arch.persistence.room.Dao;
 import android.arch.persistence.room.Insert;
 import android.arch.persistence.room.Query;
+import android.arch.persistence.room.Update;
 
 import java.lang.reflect.Member;
 import java.util.List;
@@ -14,11 +15,17 @@ public interface dao {
     @Insert
     public void addScore(Score score);
 
-    @Query("SELECT * FROM scores ORDER BY score")
+    @Query("SELECT * FROM scores ORDER BY score DESC")
     public List<Score> getScores();
 
     @Query("SELECT COUNT(*) FROM scores")
     public int getNumberOfRows();
+
+    @Query("DELETE FROM scores")
+    public void deleteAll();
+
+    @Update
+    public void changeScore(Score score);
 
 
 }
